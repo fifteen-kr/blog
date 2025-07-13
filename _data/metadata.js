bbf9f2a79fb25bfce89a11e6d498c0be9d5d877f
Jimin Park <pjm950@gmail.com>
2025-07-13 22:39:39 +0900

    템플릿 일부 수정

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,7 +3,7 @@ export default {
     title: "blog.0xF.kr",
     url: "https://blog.0xF.kr/",
     languages: "ko-KR",
-    description: "이것저것 파고드는 블로그",
+    description: "이것저것 파고드는 이상한 프로그래밍 블로그",
     author: {
         name: "JiminP",
         email: "hexagon@0xF.kr",
