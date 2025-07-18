58720d06670a6e8877be0610c498bee54a814930
Jimin Park <pjm950@gmail.com>
2025-07-18 23:41:56 +0900

    댓글 추가, 데이터 추가 등

---
+++ _data/text.js
@@ -0,0 +1,10 @@
+//@ts-check
+export default {
+    archive: "모든 게시글 목록",
+    date: "날짜",
+    title: "제목",
+
+    clickToComment: "게시글 페이지로 이동하면 댓글을 남길 수 있습니다!",
+
+    noMorePost: "모든 게시글을 다 읽었어요!",
+};
---
+++ _data/utterance.js
@@ -0,0 +1,6 @@
+//@ts-check
+export default {
+    enabled: true,
+    repo: "fifteen-kr/blog",
+    label: "\u{1F4AC} comment",
+};
