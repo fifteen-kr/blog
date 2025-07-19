b2fbc9405480a509d06382b005d24be9e208d761
Jimin Park <pjm950@gmail.com>
2025-07-20 01:45:53 +0900

    더미 Halo 클래스 생성.

---
+++ public/js/halo/index.js
@@ -0,0 +1,13 @@
+//@ts-check
+
+export class Halo {
+    /** @type {HTMLDivElement} */
+    elem;
+
+    /**
+     * @param {HTMLDivElement} elem 
+     */
+    constructor(elem) {
+        this.elem = elem;
+    }
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,10 @@
-console.log("Good morning!");
+import { Halo } from "../js/halo/index.js";
+
+function main() {
+    const halo_elem = /** @type {HTMLDivElement|null} */ (document.getElementById('header-halo'));
+    if(halo_elem) {
+        const halo = new Halo(halo_elem);
+    }
+}
+
+main();
